0cf779e3f8254724c693c7997d8c39c1b0213de6
Kars <97431994+Karscoding@users.noreply.github.com>
2023-11-22 16:55:29 +0100

    ADD PhysicsBody

---
+++ src/utility/physicsBody.cpp
@@ -0,0 +1,5 @@
+//
+// Created by karsh on 22-11-2023.
+//
+
+#include "physicsBody.h"
---
+++ src/utility/physicsBody.h
@@ -0,0 +1,14 @@
+//
+// Created by karsh on 22-11-2023.
+//
+
+#ifndef PROJECT_NAME_PHYSICSBODY_H
+#define PROJECT_NAME_PHYSICSBODY_H
+
+
+class PhysicsBody {
+
+};
+
+
+#endif //PROJECT_NAME_PHYSICSBODY_H
